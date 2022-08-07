06d31acce03948405df8cad336fb1398d6708483
Eungyu <ekpark97.dev@gmail.com>
2022-08-07 20:56:15 +0900

    test: normal arp

--- src/main.cpp
+++ src/main.cpp
@@ -31,8 +31,8 @@ int main(int argc, char* argv[]) {
 
 	EthArpPacket packet;
 
-	packet.eth_.dmac_ = Mac("00:00:00:00:00:00");
-	packet.eth_.smac_ = Mac("00:00:00:00:00:00");
+	packet.eth_.dmac_ = Mac("ff:ff:ff:ff:ff:ff");
+	packet.eth_.smac_ = Mac("00:0c:29:c2:d1:75");
 	packet.eth_.type_ = htons(EthHdr::Arp);
 
 	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
@@ -40,10 +40,10 @@ int main(int argc, char* argv[]) {
 	packet.arp_.hln_ = Mac::SIZE;
 	packet.arp_.pln_ = Ip::SIZE;
 	packet.arp_.op_ = htons(ArpHdr::Request);
-	packet.arp_.smac_ = Mac("00:00:00:00:00:00");
-	packet.arp_.sip_ = htonl(Ip("0.0.0.0"));
+	packet.arp_.smac_ = Mac("00:0c:29:c2:d1:75");
+	packet.arp_.sip_ = htonl(Ip("192.168.156.61"));
 	packet.arp_.tmac_ = Mac("00:00:00:00:00:00");
-	packet.arp_.tip_ = htonl(Ip("0.0.0.0"));
+	packet.arp_.tip_ = htonl(Ip("192.168.156.50"));
 
 	int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
 	if (res != 0) {
